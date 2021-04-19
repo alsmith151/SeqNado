7ce53cfd3dde4c543b972488675899e2bba625d2
asmith <asmith151@outlook.com>
2021-04-19 14:53:35 +0100

    Added utils.py

---
+++ chipseq_pipeline/utils.py
@@ -0,0 +1,35 @@
+def is_on(param: str) -> bool:
+    """
+    Returns True if parameter in "on" values
+    On values:
+        - true
+        - t
+        - on
+        - yes
+        - y
+        - 1
+    """
+    values = ["true", "t", "on", "yes", "y", "1"]
+    if str(param).lower() in values:
+        return True
+    else:
+        return False
+
+
+
+def is_off(param: str):
+    """Returns True if parameter in "off" values"""
+    values = ["", "None", "none", "F", "f"]
+    if str(param).lower() in values:
+        return True
+    else:
+        return False
+
+
+def is_none(param: str) -> bool:
+    """Returns True if parameter is none"""
+    values = ["", "none"]
+    if str(param).lower() in values:
+        return True
+    else:
+        return False
