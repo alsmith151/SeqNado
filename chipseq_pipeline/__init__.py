755ecf0541789991434eade5bc0ce1f1267393df
asmith <asmith151@outlook.com>
2021-04-19 14:53:20 +0100

    Added cli with click

---
+++ chipseq_pipeline/__init__.py
@@ -0,0 +1,35 @@
+import click
+import os
+import subprocess
+
+file = os.path.abspath(__file__)
+dir_package = os.path.dirname(file)
+
+
+
+
+@click.command()
+@click.option('-h', '--help', is_flag=True)
+@click.argument("mode", type=click.Choice(["make", "show", "clone", "touch"]))
+@click.argument("pipeline_options", nargs=-1, type=click.UNPROCESSED)
+def pipeline(mode, pipeline_options, help=False):
+    
+    '''Runs the data processing pipeline'''
+    
+    fn = os.path.abspath(__file__)
+    dir_cli = os.path.dirname(fn)
+    dir_package = os.path.dirname(dir_cli)
+    
+
+    cmd = ['python', 
+           f'{dir_package}/chipseq_pipeline.py', 
+           mode,
+           ]
+    
+    if help:
+        cmd.append('--help')
+    
+    if pipeline_options:
+        cmd.extend(pipeline_options)
+    
+    subprocess.run(cmd)
