20ebf0d10c7cf6df27e608f44eca62081ff74196
asmith <asmith151@outlook.com>
2021-04-19 15:17:17 +0100

    Added minimal setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+name='chipseq_pipeline',
+version='0.0.1',
+author='asmith',
+author_email='alastair.smith@ndcls.ox.ac.uk',
+packages=find_packages(),
+entry_points={'console_scripts': ['chipseq_pipeline = chipseq_pipeline:cli']},
+include_package_data=True,
+url='https://github.com/alsmith151/chipseq_pipeline',
+license='LICENSE',
+description='Chipseq pipeline based on cgat-core (ruffus)',
+long_description=open('README.md').read(),
+long_description_content_type="text/markdown",
+python_requires='>=3.8',
+install_requires=['paramiko>=2.7.1',
+                  'sqlalchemy>=1.3.18',
+                  'cgatcore>=0.6.7',
+                  'apsw',
+                  'ruffus',
+                  'drmaa',
+                  'click',
+                  'trackhub',
+                  'seaborn']
+)
