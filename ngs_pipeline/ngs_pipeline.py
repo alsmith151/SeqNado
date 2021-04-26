229def5cdc8b2740ce86f372ccb5812b1379b45e
alsmith <asmith151@outlook.com>
2021-04-26 11:22:04 +0100

    Typo in replace

--- ngs_pipeline/ngs_pipeline.py
+++ ngs_pipeline/ngs_pipeline.py
@@ -351,7 +351,7 @@ def alignments_pileup(infile, outfile):
 def call_peaks(infile, outfile):
 
     peaks_options = P.PARAMS.get("peaks_options")
-    output_prefix = outfile.replace('.narrowPeak')
+    output_prefix = outfile.replace('.narrowPeak', '')
     statement = "%(peaks_caller)s callpeak -t %(infile)s -n %(output_predix)s "
 
     chipseq_match = re.match(r".*/(.*)_(.*).bam", infile)
