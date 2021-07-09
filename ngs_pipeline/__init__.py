e674a399fad1426983bd22eb598151ea9d163703
alsmith151 <49727900+alsmith151@users.noreply.github.com>
2021-07-09 13:05:28 +0100

    Major pipeline updates (#10)
    * Duplicated config files
    
    * Moved misc scripts
    
    * Renamed pipeline
    
    * Will now accept a more generic config.yml file name
    
    * Added RNA pipeline
    
    * Small changes to RNA pipeline
    
    * Minor formating
    
    * Updates
    
    * Rename of yaml
    
    * Added RNA pipeline to cli
    
    * FeatureCounts to lowercase
    
    * Increased job memory
    
    * Updated alignment task
    
    * Added support for single end sequencing
    
    * Updated README.md
    
    * Fixed old task name
    
    * Added single end file
    
    * Updated README.md
    
    * Renamed test
    
    * Renamed
    
    * Added missing option
    
    * Updated older parameters
    
    * Need to ignore trimmed part of file name
    
    * Removed .gz ext
    
    * Sorting before converting to bigbed
    
    * Added test fasta
    
    * Added test for chip pipeline
    
    * Fixed issues with homer bigwig generation
    
    * Correct genome size
    
    * Ensure that crashes occur during testing by raising exception
    
    * Macs peak calling on by default
    
    * Will have a single and paired end test
    
    * Added single end test
    
    * Adjusted single trimming regex to cope with ATAC naming
    
    * Added test for ATAC
    
    * Added test file for single end ChIP
    
    * Added hg19 gtf file. Just chr21
    
    * Updated rna config file
    
    * Updated config
    
    * Updated config further
    
    * Changed command
    
    * Updated test
    
    * Updated test
    
    * Removed old joint test
    
    * Modified trimming regex
    
    * Typo in indicies location
    
    * Added star options
    
    * Older run parameters swapped for newer
    
    * Added packages required for RNA pipeline
    
    * Modified bigwig output regex
    
    * Updated test
    
    * Removed old test data

--- ngs_pipeline/__init__.py
+++ ngs_pipeline/__init__.py
@@ -8,16 +8,19 @@ dir_package = os.path.dirname(file)
 
 @click.command(context_settings=dict(ignore_unknown_options=True))
 @click.option('-h', '--help', is_flag=True)
+@click.argument("method", type=click.Choice(['atac', 'chip', 'rna']))
 @click.argument("mode", type=click.Choice(["make", "show", "clone", "touch"]))
 @click.argument("pipeline_options", nargs=-1, type=click.UNPROCESSED)
-def cli(mode, pipeline_options, help=False):
+def cli(method, mode, pipeline_options, help=False):
     
     '''Runs the data processing pipeline'''   
 
-    cmd = ['python', 
-           f'{dir_package}/ngs_pipeline.py', 
-           mode,
-           ]
+    if method == 'chip' or method == 'atac':
+        cmd = ['python', f'{dir_package}/pipeline_atac_chipseq.py', mode,]
+    else:
+        cmd = ['python', f'{dir_package}/pipeline_rna.py', mode]
+
+
     
     if help:
         cmd.append('--help')
@@ -25,4 +28,7 @@ def cli(mode, pipeline_options, help=False):
     if pipeline_options:
         cmd.extend(pipeline_options)
     
-    subprocess.run(cmd)
+    completed = subprocess.run(cmd)
+
+    if not completed.returncode == 0:
+        raise RuntimeError("Pipeline failed. Check the log.")
