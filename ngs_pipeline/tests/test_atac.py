d14cc9b1656807390e1f64f89fee497a1f204b2f
alsmith <asmith151@outlook.com>
2021-07-09 12:14:11 +0100

    Changed command

--- ngs_pipeline/tests/test_atac.py
+++ ngs_pipeline/tests/test_atac.py
@@ -66,7 +66,7 @@ def set_up():
 
 def test_pipeline():
 
-    cmd = "ngs-pipeline chip make --local -p 4"
+    cmd = "ngs-pipeline atac make --local -p 4"
     completed = subprocess.run(cmd.split())
     assert completed.returncode == 0
    
