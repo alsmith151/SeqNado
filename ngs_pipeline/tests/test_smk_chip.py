caf8ae114d42e9952766976270b8c9e067595e8a
alsmith <asmith151@outlook.com>
2022-05-25 17:37:53 +0100

    Updated snakemake chip pipeline test

--- ngs_pipeline/tests/test_smk_chip.py
+++ ngs_pipeline/tests/test_smk_chip.py
@@ -8,54 +8,79 @@ import glob
 import shutil
 import subprocess
 
-path_file = os.path.abspath(__file__)
-dir_tests = os.path.dirname(path_file)
-dir_tests_run = os.path.join(dir_tests, "pipeline_chip_single")
-dir_package = os.path.dirname(dir_tests)
-dir_repo = os.path.dirname(dir_package)
-dir_data_test = os.path.join(dir_package, "data", "test")
-dir_data_genome = os.path.join(dir_package, "data", "genome")
 
 @pytest.fixture(scope="module")
-def pipeline_path():
-    return os.path.join(dir_package, "chipseq", "snakefile")
+def package_path():
+
+    path_file = os.path.abspath(__file__)
+    path_test = os.path.dirname(path_file)
+    path_package = os.path.dirname(path_test)
+    return path_package
 
 
-@pytest.fixture(scope="module", autouse=True)
-def set_up():
+@pytest.fixture(scope="module")
+def repo_path(package_path):
+    return os.path.dirname(package_path)
 
-    path_root = os.getcwd()
 
-    # Make indicies
-    indicies = os.path.join(dir_data_genome, 'bt2')
-    fasta = os.path.join(dir_data_genome, "chr21_rename.fa")
+@pytest.fixture(scope="module")
+def pipeline_path(package_path):
+    return os.path.join(package_path, "chipseq", "snakefile")
+
+
+@pytest.fixture(scope="module")
+def data_path(package_path):
+    return os.path.join(package_path, "data")
+
+
+@pytest.fixture(scope="module")
+def chromsizes(data_path):
+    return os.path.join(data_path, "genome", "chr21_rename.fa.fai")
+
+
+@pytest.fixture(scope="module")
+def genome_indicies(data_path):
+    indicies = os.path.join(data_path, "genome", "bt2")
+
     if not os.path.exists(indicies):
+
+        fasta = os.path.join(data_path, "genome", "chr21_rename.fa")
         os.mkdir(indicies)
         cmd = f"bowtie2-build {fasta} {indicies}/chr21 --threads 8"
         subprocess.run(cmd.split())
-    
-    
-    # Change wd
-    if not os.path.exists(dir_tests_run):
-        os.mkdir(dir_tests_run)
-    
-    os.chdir(dir_tests_run)
+
+    return indicies
+
+
+@pytest.fixture(scope="module")
+def run_directory(tmpdir_factory):
+    fn = tmpdir_factory.mktemp("data")
+    return fn
+
+
+@pytest.fixture(scope="module", autouse=True)
+def set_up(
+    run_directory, data_path, package_path, repo_path, genome_indicies, chromsizes
+):
+
+    cwd = os.getcwd()
+    os.chdir(run_directory)
 
     # Move config files and fastq files
-    for fq in glob.glob(f"{dir_data_test}/SINGLE*.fastq.gz"):
+    for fq in glob.glob(f"{data_path}/test/*CTCF*.fastq.gz"):
         shutil.copy(fq, ".")
-    
+
     # Move and replace the config file
     replacements_dict = {
         "GENOME_NAME": "hg19",
-        "CHROM_SIZES": f"{fasta}.fai",
-        "PATH_TO_ALIGNER_INDICIES": f"{indicies}/chr21",
-        "HUB_DIR": dir_tests_run,
+        "CHROM_SIZES": f"{chromsizes}",
+        "PATH_TO_ALIGNER_INDICIES": f"{genome_indicies}/chr21",
+        "HUB_DIR": f"{run_directory}/UCSC_HUB/",
         "BAM_COVERAGE_OPTIONS": "",
-        "MACS_OPTIONS": "-g 46709983"
+        "MACS_OPTIONS": "-g 46709983",
     }
 
-    with open(f"{dir_repo}/config_chip.yml", "r") as r:
+    with open(f"{repo_path}/config_chip.yml", "r") as r:
         with open("config_chip.yml", "w") as w:
             for line in r:
                 for rep_key in replacements_dict:
@@ -66,14 +91,13 @@ def set_up():
 
     yield
 
-    os.chdir(path_root)
-    breakpoint()
-    shutil.rmtree(dir_tests_run)
+    os.chdir(cwd)
 
 
 def test_pipeline(pipeline_path):
 
-    cmd = f"snakemake --cores 4 --snakefile {pipeline_path} --configfile config_chip.yml"
+    cmd = (
+        f"snakemake --cores 8 --snakefile {pipeline_path} --configfile config_chip.yml"
+    )
     completed = subprocess.run(cmd.split())
     assert completed.returncode == 0
-   
