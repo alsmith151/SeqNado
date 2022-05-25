a5dca7f1dd97e5b9e3b062505451b90a552bc509
alsmith <asmith151@outlook.com>
2022-05-25 17:37:18 +0100

    Fixed chipseq errors

--- ngs_pipeline/chipseq/utils.py
+++ ngs_pipeline/chipseq/utils.py
@@ -5,6 +5,7 @@ import pybedtools
 import pandas as pd
 import re
 import glob
+import numpy as np
 
 
 def set_up_chromsizes(config: Dict):
@@ -36,61 +37,6 @@ def set_up_chromsizes(config: Dict):
     print(f"Obtained chromosome sizes for genome {config['genome']['name']}")
 
 
-def get_fastq_files(path: os.PathLike) -> pd.DataFrame:
-    fq_list = glob.glob(path)
-    sample_info = pd.DataFrame(fq_list, columns=["fq_files"])
-    sample_info["fq"] = [os.path.basename(fn) for fn in sample_info["fq_files"]]
-
-    sample_info["is_single_end"] = sample_info["fq"].str.match(r"(.*?).fastq(.gz)?")
-    sample_info["is_paired_end"] = sample_info["fq"].str.match(
-        r"(.*)_R?[12].fastq(.gz)?"
-    )
-
-    sample_info_single = sample_info.loc[sample_info["is_single_end"]]
-    sample_info_paired = sample_info.loc[sample_info["is_paired_end"]]
-
-    sample_info_single["sample_name"] = sample_info_single["fq"].str.extract(
-        "(.*?).fastq(?!.gz)?"
-    )
-    sample_info_single["read"] = 0
-
-    sample_info_paired["sample_name"] = sample_info_paired["fq"].str.extract(
-        "(.*?)_R?\d.fastq(?!.gz)?"
-    )
-    sample_info_paired["read"] = (
-        sample_info_paired["fq"].str.extract(".*?_R?(\d).fastq(?!.gz)?").astype(int)
-    )
-
-    return pd.concat([sample_info_single, sample_info_paired])
-
-
-def extract_chip_sample_info(sample_info: pd.DataFrame):
-
-    # Ensure that all samples match the naming convention
-    if (
-        not sample_info["fq"]
-        .str.match("(?P<sample>.*)_(?P<antibody>.*?)(_R?[12])?.fastq(.gz)?")
-        .all()
-    ):
-        raise ValueError("Sample names do not match the naming convention.")
-
-    # Extract sample names and antibody names
-    sample_info[["sample_name", "antibody"]] = sample_info["fq"].str.extract(
-        "(?P<sample>.*?)_(?P<antibody>.*?)(?!_R?[12])?.fastq(?!.gz)?"
-    )
-    sample_info["antibody"] = sample_info["antibody"].str.split("_[12]").str[0]
-
-    sample_info["is_input"] = sample_info["sample_name"].str.contains(
-        "input", case=False
-    )
-
-    return {
-        "info": sample_info,
-        "ip": sample_info.query("is_input == False")["fq_files"].to_list(),
-        "input": sample_info.query("is_input == True")["fq_files"].to_list(),
-    }
-
-
 def symlink_fastq_files(sample_info: pd.DataFrame):
 
     try:
@@ -99,8 +45,88 @@ def symlink_fastq_files(sample_info: pd.DataFrame):
         pass
 
     for fq in sample_info.itertuples():
-        full_path = pathlib.Path(fq.fq_files).absolute()
-        symlink_path = f"fastq/{fq.sample_name}_{fq.antibody}_R{fq.read}.fastq.gz"
+        full_path = pathlib.Path(fq.fn).absolute()
+        symlink_path = f"fastq/{fq.basename}"
 
         if not os.path.exists(symlink_path):
             os.symlink(full_path, symlink_path)
+
+
+def get_fastq_files(path: str) -> pd.DataFrame:
+    df = pd.DataFrame(data=pathlib.Path(".").glob("*.fastq.gz"), columns=["fn"])
+    df = df.assign(basename=lambda df: df["fn"].apply(lambda p: p.name))
+
+    df["paired_or_single"] = df["basename"].str.match(r"(.*)_R?[12].fastq(.gz)?")
+    df["paired_or_single"] = np.where(
+        df["paired_or_single"] == True, "paired", "single"
+    )
+    return df
+
+
+def sample_names_follow_convention(
+    df: pd.DataFrame, name_column: str = "basename"
+) -> bool:
+    naming_pattern_paired = r"(.*)_(.*)_R?[12].fastq(.gz)?"
+    naming_pattern_single = r"(.*)_(.*).fastq(.gz)?"
+
+    return (
+        df[name_column].str.match(naming_pattern_paired)
+        | df[name_column].str.match(naming_pattern_single)
+    ).all()
+
+
+def get_sample_attributes(df: pd.DataFrame):
+
+    if not df.empty:
+        if not sample_names_follow_convention(df):
+            raise ValueError(
+                "Sample names do not follow the naming convention. Use a design matrix to specify inputs"
+            )
+
+    df_paired = df.query("paired_or_single == 'paired'")
+    df_single = df.query("paired_or_single == 'single'")
+
+    df_paired = df_paired.join(
+        df_paired["basename"].str.extract(
+            r"(?P<sample_name>.*?)_(?P<antibody_or_input>.*?)_(?P<read_number>[12]).fastq.gz"
+        )
+    )
+
+    df_single = df_single.join(
+        df_single["basename"]
+        .str.extract(r"(?P<sample_name>.*?)_(?P<antibody_or_input>.*?).fastq.gz")
+        .assign(read_number=0)
+    )
+
+    return pd.concat([df_paired, df_single]).assign(
+        fn=df["fn"].astype(str),
+        basename=df["basename"],
+        paired=df["paired_or_single"].str.contains("paired"),
+    )
+
+
+def has_ambigous_inputs(df: pd.DataFrame) -> bool:
+    return (
+        df.drop_duplicates(subset=["sample_name", "antibody", "input"])
+        .duplicated(subset=["sample_name", "input"])
+        .any()
+    )
+
+
+def pair_inputs_with_samples(df: pd.DataFrame):
+
+    df_input = df.query("antibody_or_input.str.contains('input', case=False)")
+    df_ip = df.query("~antibody_or_input.str.contains('input', case=False)")
+
+    df_ip_with_input = df_ip.merge(
+        df_input[["sample_name", "antibody_or_input"]], on="sample_name", how="left"
+    ).rename(
+        columns={"antibody_or_input_x": "antibody", "antibody_or_input_y": "input"}
+    )
+
+    if not has_ambigous_inputs(df_ip_with_input):
+        return df_ip_with_input
+    else:
+        raise ValueError(
+            "Samples are paired with more than one input. Use a design matrix to specify inputs"
+        )
