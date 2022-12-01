41f30964de01f932625a1f39f7ee0f6590c367f0
Alastair Smith <49727900+alsmith151@users.noreply.github.com>
2022-12-01 16:57:13 +0000

    BREAKING CHANGE: Replace cgat-core with snakemake (#25)
    * Started pipeline re-write in snakemake
    
    * Completed chipseq pipeline in snakemake
    
    * Fixed chipseq errors
    
    * Updated snakemake chip pipeline test
    
    * Updated chip pipeline code
    
    * Bug fix for pipeline interaction with singularity
    
    * Updated setup
    
    * Pinned samtools dependency
    
    * Fixed QC bugs and other minor bugs
    
    * Updated config_chip.yml and qc.smk
    
    * Updated pipeline with new test
    
    * Fixed wrong input parameter in multiqc_bam-X
    
    * Allowed both single and paired end bigwigs to be made simultaneously
    
    * Added empty blacklist parameter
    
    * Removed commented section
    
    * Updated ChIP pipeline test for snakemake
    
    * Moved old tests
    
    * Moved old tests
    
    * Updated rules
    
    * Updated setup
    
    * Updated packaging
    
    * Commented out chromsizes
    
    * Updated config files and setup.cfg
    
    * Added all pipelines to cli
    
    * Updated use_singularity function
    
    * Updated pairing utilities
    
    * Updated snakemake rules for pipeline with fixes
    
    * Added ASSAY global variable
    
    * Added ATAC snakefile
    
    * Moved shfit_alignments into filter_alignments
    
    * Added default profile for the cluster
    
    * Small updates to chipseq config and rules
    
    * updated container
    
    * Updated sample specification
    
    * Updated generic rules
    
    * Changed sample specification functions
    
    * Updated setup.cfg
    
    * Added design key
    
    * Removed symlink fastq files
    
    * Improved sample specification
    
    * Updated rules to cope with new specification
    
    * Updated test for just paired samples temporarily
    
    * Updated conda env
    
    * Partial update to readme
    
    * Renamed tests
    
    * Corrected fastqc_raw input specification
    
    * Updated workflow to include singularity/apptainer
    
    * Moved config files to separate folder
    
    * Added profiles into package data for use in pipe
    
    * Added test data to tests folder
    
    * Updated manifest to include profiles
    
    * Updated readme. Still needs editing
    
    * Removed old test data and tests
    
    * Fixed a bug in control files symlink if NA
    
    * Slow re-work of cli
    
    * Updated actions workflow
    
    * Updated pipeline with atac samples
    
    * Added RNA pipeline
    
    * Updates for rna pipeline integration
    
    * Updated align and post align rules
    
    * Added rna pipeline test
    
    * Updated workflow
    
    * Workflow update
    
    * Update python version
    
    * Using rsbamtk for atac shift
    
    * Updated ChIP-seq snakefile
    
    * fix: atac shift not running
    
    * Removed rule
    
    * fix: Fixed star genome dir generation fixture
    
    * Updated rules
    
    * Re-indexed file after shifting
    
    * Updated RNA-seq hub
    
    * Updated rna hub making
    
    * Added bam index to filtered bam
    
    * Moved bamtk to optional
    
    * Renamed index rule
    
    * Made star log
    
    * Updated STAR resources
    
    * Updated gitignore
    
    * removed peak call methods from RNA
    
    * Removed pybedtools from package
    
    * Installed the atac optional packages
    
    * Fixed RNA-seq KeyError
    
    * Commented out index_filtered_bam

---
+++ tests/test_chip.py
@@ -0,0 +1,162 @@
+import glob
+import os
+import shutil
+import subprocess
+
+import pytest
+
+
+@pytest.fixture(scope="module")
+def repo_path():
+    path_file = os.path.abspath(__file__)
+    path_test = os.path.dirname(path_file)
+    path_repo = os.path.dirname(path_test)
+    return path_repo
+
+
+@pytest.fixture(scope="module")
+def package_path(repo_path):
+    return os.path.join(repo_path, "ngs_pipeline")
+
+
+@pytest.fixture(scope="module")
+def pipeline_path(package_path):
+    return os.path.join(package_path, "chipseq", "snakefile")
+
+
+@pytest.fixture(scope="module")
+def test_dir_path(repo_path):
+    return os.path.join(repo_path, "tests")
+
+
+@pytest.fixture(scope="module")
+def data_path(test_dir_path):
+    return os.path.join(test_dir_path, "data")
+
+
+@pytest.fixture(scope="module")
+def genome_path(data_path):
+    return os.path.join(data_path, "genome")
+
+
+@pytest.fixture(scope="module")
+def chromsizes(genome_path):
+    return os.path.join(genome_path, "chr21_rename.fa.fai")
+
+
+@pytest.fixture(scope="module")
+def fastqs(data_path):
+    path = os.path.join(data_path, "fastq")
+    return glob.glob(os.path.join(path, "*CTCF*_*_*.fastq.gz"))
+
+
+@pytest.fixture(scope="module")
+def config_path(repo_path):
+    return os.path.join(repo_path, "config")
+
+
+@pytest.fixture(scope="module")
+def genome_indicies(genome_path):
+
+    indicies = os.path.join(genome_path, "bt2")
+
+    if not os.path.exists(indicies):
+        try:
+            import requests
+            import tarfile
+
+            url = (
+                "https://userweb.molbiol.ox.ac.uk/public/asmith/ngs_pipeline/bt2.tar.gz"
+            )
+            output = os.path.join(genome_path, "bt2.tar.gz")
+            r = requests.get(url, stream=True)
+            with open(output, "wb") as f:
+                f.write(r.content)
+
+            tar = tarfile.open(output)
+            tar.extractall(path=genome_path)
+            tar.close()
+            os.remove(output)
+            os.rename(genome_path + "/bt2", indicies)
+
+        except Exception as e:
+            print(e)
+            print("Could not download indicies so generating them")
+            os.mkdir(indicies)
+            cmd = f"bowtie2-build {os.path.join(genome_path,'chr21_rename.fa')} {indicies}/bt2 --threads 8"
+            subprocess.run(cmd.split())
+
+    return os.path.join(indicies, "chr21")
+
+
+@pytest.fixture(scope="module")
+def run_directory(tmpdir_factory):
+    fn = tmpdir_factory.mktemp("data")
+    return fn
+
+
+@pytest.fixture(scope="module", autouse=True)
+def set_up(
+    run_directory,
+    data_path,
+    package_path,
+    repo_path,
+    genome_indicies,
+    chromsizes,
+    fastqs,
+    config_path,
+):
+
+    cwd = os.getcwd()
+    os.chdir(run_directory)
+
+    # Move config files and fastq files
+    for fq in fastqs:
+        shutil.copy(fq, ".")
+
+    # Move and replace the config file
+    replacements_dict = {
+        "GENOME_NAME": "hg19",
+        "CHROMOSOME_SIZES_FILE": chromsizes,
+        "INDICES_DIRECTORY": genome_indicies,
+        "HUB_DIRECTORY_PATH": f"{run_directory}/UCSC_HUB/",
+        "MACS_OPTIONS": "-g 46709983",
+    }
+
+    with open(f"{config_path}/config_chip.yml", "r") as r:
+        with open("config_chip.yml", "w") as w:
+            for line in r:
+                for rep_key in replacements_dict:
+                    if rep_key in line:
+                        line = line.replace(rep_key, replacements_dict[rep_key])
+
+                w.write(line)
+
+    yield
+
+    os.chdir(cwd)
+
+
+def test_pipeline_conda():
+
+    cmd = f"ngs-pipeline chip --cores 4 --configfile config_chip.yml"
+    completed = subprocess.run(cmd.split())
+    assert completed.returncode == 0
+
+
+def test_pipeline_singularity(genome_path):
+    indicies_dir = os.path.join(genome_path, "bt2")
+
+    cmd = [
+        "ngs-pipeline",
+        "chip",
+        "--cores",
+        "4",
+        "--configfile",
+        "config_chip.yml",
+        "--use-singularity",
+        "--singularity-args",
+        f'" -B {indicies_dir} -B {genome_path}"',
+    ]
+    completed = subprocess.run(" ".join(cmd), shell=True)
+    assert completed.returncode == 0
---
+++ tests/test_rna.py
@@ -0,0 +1,170 @@
+import glob
+import os
+import shutil
+import subprocess
+
+import pytest
+
+
+@pytest.fixture(scope="module")
+def repo_path():
+    path_file = os.path.abspath(__file__)
+    path_test = os.path.dirname(path_file)
+    path_repo = os.path.dirname(path_test)
+    return path_repo
+
+
+@pytest.fixture(scope="module")
+def package_path(repo_path):
+    return os.path.join(repo_path, "ngs_pipeline")
+
+
+@pytest.fixture(scope="module")
+def pipeline_path(package_path):
+    return os.path.join(package_path, "chipseq", "snakefile")
+
+
+@pytest.fixture(scope="module")
+def test_dir_path(repo_path):
+    return os.path.join(repo_path, "tests")
+
+
+@pytest.fixture(scope="module")
+def data_path(test_dir_path):
+    return os.path.join(test_dir_path, "data")
+
+
+@pytest.fixture(scope="module")
+def genome_path(data_path):
+    return os.path.join(data_path, "genome")
+
+
+@pytest.fixture(scope="module")
+def chromsizes(genome_path):
+    return os.path.join(genome_path, "chr21_rename.fa.fai")
+
+
+@pytest.fixture(scope="module")
+def fastqs(data_path):
+    path = os.path.join(data_path, "fastq")
+    return glob.glob(os.path.join(path, "rna*.fastq.gz"))
+
+
+@pytest.fixture(scope="module")
+def config_path(repo_path):
+    return os.path.join(repo_path, "config")
+
+
+@pytest.fixture(scope="module")
+def genome_indicies(genome_path):
+
+    indicies = os.path.join(genome_path, "GenomeDir")
+    gtf = os.path.join(genome_path, "chr21.gtf")
+    fasta = os.path.join(genome_path, "chr21_rename.fa")
+
+    if not os.path.exists(indicies):
+        try:
+            import requests
+            import tarfile
+
+            url = "https://userweb.molbiol.ox.ac.uk/public/asmith/ngs_pipeline/star.tar.gz"
+            output = os.path.join(genome_path, "star.tar.gz")
+            r = requests.get(url, stream=True)
+            with open(output, "wb") as f:
+                f.write(r.content)
+
+            tar = tarfile.open(output)
+            tar.extractall(path=genome_path)
+            tar.close()
+            os.remove(output)
+            os.rename(os.path.join(genome_path, "GenomeDir"), indicies)
+
+        except Exception as e:
+            print(e)
+            print("Could not download indicies so generating them")
+            os.mkdir(indicies)
+            cmd = f"""STAR 
+                  --runMode genomeGenerate
+                  --runThreadN 4 
+                  --genomeDir {indicies}
+                  --genomeFastaFiles {fasta}
+                  --sjdbGTFfile {gtf} 
+                  --sjdbOverhang 100 
+                  --genomeSAindexNbases 11 
+                  """
+            subprocess.run(cmd.split())
+
+    return indicies
+
+
+@pytest.fixture(scope="module")
+def run_directory(tmpdir_factory):
+    fn = tmpdir_factory.mktemp("data")
+    return fn
+
+
+@pytest.fixture(scope="module", autouse=True)
+def set_up(
+    run_directory,
+    data_path,
+    package_path,
+    repo_path,
+    genome_path,
+    genome_indicies,
+    chromsizes,
+    fastqs,
+    config_path,
+):
+
+    cwd = os.getcwd()
+    os.chdir(run_directory)
+
+    # Move config files and fastq files
+    for fq in fastqs:
+        shutil.copy(fq, ".")
+
+    # Move and replace the config file
+    replacements_dict = {
+        "GENOME_NAME": "hg19",
+        "CHROMOSOME_SIZES_FILE": chromsizes,
+        "INDICES_DIRECTORY": genome_indicies,
+        "HUB_DIRECTORY_PATH": f"{run_directory}/UCSC_HUB/",
+        "GTF": os.path.join(genome_path, "chr21.gtf"),
+    }
+
+    with open(f"{config_path}/config_rna.yml", "r") as r:
+        with open("config_rna.yml", "w") as w:
+            for line in r:
+                for rep_key in replacements_dict:
+                    if rep_key in line:
+                        line = line.replace(rep_key, replacements_dict[rep_key])
+
+                w.write(line)
+
+    yield
+
+    os.chdir(cwd)
+
+
+def test_pipeline_conda():
+
+    cmd = f"ngs-pipeline rna --cores 4 --configfile config_rna.yml"
+    completed = subprocess.run(cmd.split())
+    assert completed.returncode == 0
+
+
+def test_pipeline_singularity(genome_path, genome_indicies):
+
+    cmd = [
+        "ngs-pipeline",
+        "rna",
+        "--cores",
+        "4",
+        "--configfile",
+        "config_rna.yml",
+        "--use-singularity",
+        "--singularity-args",
+        f'" -B {genome_indicies} -B {genome_path}"',
+    ]
+    completed = subprocess.run(" ".join(cmd), shell=True)
+    assert completed.returncode == 0
