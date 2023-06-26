8632adafc9190ae0265ee81dfdd981c85a88a5e5
alsmith <asmith151@outlook.com>
2023-06-26 19:13:02 +0100

    fix: typo in deseq2 key

--- seqnado/utils.py
+++ seqnado/utils.py
@@ -352,7 +352,7 @@ def define_output_files(
             )
         
         if kwargs["run_deseq2"]:
-            project_id = kwargs["DESeq2"].get("project_id")
+            project_id = kwargs["deseq2"].get("project_id")
             assay_output.append(f"DESeq2_{project_id}.html") 
 
 
