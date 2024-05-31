34661ba62897630d0f9cf3b2c44e3c17c8d8b3a5
CChahrour <catherine.chahrour@gmail.com>
2024-05-31 15:07:45 +0100

    correct apptainer cache dir in tests

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -412,13 +412,15 @@ def apptainer_args(indicies, test_data_path):
     indicies_mount = indicies.parent if not indicies.is_dir() else indicies
     tmpdir = pathlib.Path(os.environ.get("TMPDIR", "/tmp") or "/tmp")
     wd = pathlib.Path(os.getcwd()).resolve()
+    apptainer_cache_dir = pathlib.Path.home() / ".apptainer"
+    
     os.environ["APPTAINER_BINDPATH"] = (
         f"{wd}:{wd},"
         f"{test_data_path}:{test_data_path},"
         f"{indicies_mount}:{indicies_mount},"
         f"{tmpdir}:{tmpdir}"
     )
-    os.environ["APPTAINER_CACHEDIR"] = f"{tmpdir}"
+    os.environ["APPTAINER_CACHEDIR"] = str(apptainer_cache_dir)
 
 
 @pytest.fixture(scope="function")
