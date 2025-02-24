bdc04e7e51a558ef6120219b4eabe4a023e04884
Alastair Smith <49727900+alsmith151@users.noreply.github.com>
2025-02-24 18:20:26 +0000

    feat(containers): add container definitions to repo (#240)
    * feat(containers): add apptainer definition files and envs
    
    * fix(containers): update Quarto version and improve cleanup process in seqnado_report.def
    
    * feat(containers): add build script and update environment dependencies for improved container setup
    
    * fix(containers): remove unnecessary fakeroot option from container build script
    
    * fix(containers): change uninstall command to remove for clarity in seqnado_report.def
    
    * fix(containers): streamline cleanup process by removing unnecessary commands in seqnado_report.def
    
    * fix(setup): exclude containers directory from package discovery in pyproject.toml
    
    * fix(setup): update package discovery to include only the seqnado package in pyproject.toml

---
+++ containers/build_containers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set env variables
+export APPTAINER_BINDPATH="" # Don't bind any paths as this causes errors
+
+# Build all containers
+for container in $(ls *.def);
+do
+    echo "Building container $container"
+    apptainer build $(basename $container .def).sif $container
+done
