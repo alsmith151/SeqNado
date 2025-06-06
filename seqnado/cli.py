d959bf1ac32bd2641cf0a30b64528e1e4c4e6fbb
Alastair Smith <49727900+alsmith151@users.noreply.github.com>
2025-06-06 12:27:42 +0100

    Feat crispr workflow (#267)
    * feat(crispr): add support for CRISPR assay including configuration and workflow rules
    
    * fix(crispr): remove options for bowtie2 in tool configuration
    
    * feat(crispr): add trimming and alignment rules for CRISPR workflow
    
    * feat(crispr): add cutadapt configuration for trimming options
    
    * fix(crispr): remove redundant option for featurecounts in tool configuration
    
    * fix(crispr): remove trim_galore configuration from tool options
    
    ---------
    
    Co-authored-by: CChahrour <catherine.chahrour@gmail.com>

--- seqnado/cli.py
+++ seqnado/cli.py
@@ -10,7 +10,7 @@ from loguru import logger
 
 FILE = os.path.abspath(__file__)
 PACKAGE_DIR = os.path.dirname(FILE)
-ASSAYS = ["atac", "chip", "rna", "snp", 'cat', 'meth', 'mcc']
+ASSAYS = ["atac", "chip", "rna", "snp", 'cat', 'meth', 'mcc', 'crispr']
 
 
 @click.command(context_settings=dict(ignore_unknown_options=True))
