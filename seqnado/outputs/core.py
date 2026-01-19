90925fd07ee391f8d39b11d1d0fb15d2d2e683b4
alsmith151 <alastair.smith@ndcls.ox.ac.uk>
2026-01-19 16:27:09 +0000

    fix: update file selection criteria for genome browser and sentinel files

--- seqnado/outputs/core.py
+++ seqnado/outputs/core.py
@@ -1,6 +1,7 @@
 from itertools import chain
 from pathlib import Path
 from typing import Any, List
+from xml.etree.ElementInclude import include
 
 from loguru import logger
 from pydantic import BaseModel, Field
@@ -173,11 +174,11 @@ class SeqnadoOutputFiles(BaseModel):
 
     @property
     def genome_browser_plots(self):
-        return self.select_files(".pdf", contains="genome_browser")
+        return self.select_files(".pdf", include="genome_browser")
     
     @property
     def sentinel_files(self):
-        return self.select_files(".txt", contains=".mcc_")
+        return self.select_files(".txt", include=".mcc_")
 
     @property
     def ucsc_hub_files(self):
